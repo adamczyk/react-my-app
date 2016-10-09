8b0cc2656d5dfc4bbf8d8691ad8260d1bf6d26d9
Kamil Adamczyk <kamiladamczyk@hotmail.com>
2016-10-09 18:26:10 +0200

    Test git

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>Hello, World!</h1>
+        <h2>Test git</h2>
       </div>
     );
   }
