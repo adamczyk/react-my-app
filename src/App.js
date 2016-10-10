5b19a21297912a553f897c39f196b65d5db17365
Kamil Adamczyk <kamiladamczyk@hotmail.com>
2016-10-10 14:57:21 +0200

    First day of playing with React

--- src/App.js
+++ src/App.js
@@ -4,20 +4,114 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var TABLE_CONTENT = [
+     {title: "Audi", subTitle: "A6"},
+     {title: 'VW', subTitle: "Passat"},
+     {title: 'Ferrari', subTitle: "456 Italia"}
+    ]
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
         <h1>Hello, World!</h1>
-        <h2>Test git</h2>
+        <ListOfItems tableContent={TABLE_CONTENT} />
+        {/*
+        <MyForm />
+        <ResizableImg />
+        */}
       </div>
     );
   }
 }
 
 export default App;
+
+class ResizableImg extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      height: 100
+    }
+  }
+
+  handleClick(delta) {
+    if((this.state.height+delta) >= 20) {
+      this.setState({
+        height: this.state.height + delta
+      })
+    }
+  }
+
+  render() {
+    var { height } = this.state
+    var localStyle = {
+      borderStyle: 'solid',
+      borderWidth: '1px',
+      borderColor: 'red'
+    }
+    return (
+      <div style={localStyle}>
+        <button onClick={this.handleClick.bind(this, -20)}>-</button>
+        <button onClick={this.handleClick.bind(this, 20)}>+</button>
+        <br />
+        <img src={logo} height={height}/>
+      </div>
+    )
+  }
+}
+
+class MyForm extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      inputValue: "hello"
+    }
+  }
+
+  handleChange(event) {
+    this.setState({inputValue: event.target.value})
+  }
+
+  render() {
+    var { inputValue } = this.state
+    return (
+      <div>
+        <input type="text" value={inputValue} onChange={this.handleChange.bind(this)}/>
+        <h4>{inputValue}</h4>
+      </div>
+    )
+  }
+}
+
+class ListOfItems extends Component {
+  render() {
+    return (
+      <ul>
+          {this.props.tableContent.map((result, i) => (
+            <Item key={i} title={result.title} subTitle={result.subTitle} />
+          ))}
+      </ul>
+    )
+  }
+}
+ListOfItems.propTypes = {
+  tableContent: React.PropTypes.array
+}
+
+class Item extends Component {
+  render() {
+    return (
+      <li>
+        <h2>{this.props.title}</h2>
+        <h4>{this.props.subTitle}</h4>
+      </li>
+    )
+  }
+}
+Item.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  subTitle: React.PropTypes.string.isRequired
+}
+Item.defaultProps = {
+  title: "Title",
+  subTitle: "SubTitle"
+}
